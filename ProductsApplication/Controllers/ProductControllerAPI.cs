0443fbeb08161b4dc99e0f5cac5f011c2d5249c7
BuildTools <wafaaelayoubi.w@gmail.com>
2022-08-26 19:58:31 +0300

    Adding API and DTO

---
+++ ProductsApplication/Controllers/ProductControllerAPI.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http.Description;
+using Bogus;
+using Microsoft.AspNetCore.Mvc;
+using ProductsApplication.Models;
+using ProductsApplication.Services;
+
+namespace ProductsApplication.Controllers
+{
+    [ApiController]
+    [Route("api/")]
+    public class ProductControllerAPI : ControllerBase
+    {
+        ProductsDAO repository;
+        public ProductControllerAPI()
+        {
+            repository = new ProductsDAO();
+        }
+
+        [HttpGet]
+        //this is bc we returned IEnumerable
+        [ResponseType(typeof(List<ProductModelDTO>))]
+        public IEnumerable<ProductModelDTO> Index()
+        {
+            List<ProductModel> products = repository.GetAllProducts();
+            IEnumerable<ProductModelDTO> productModelDTOs = from p in products select new ProductModelDTO(p);
+
+            return productModelDTOs;
+        }
+
+        //Another way that i like
+        //[HttpGet]
+        //public ActionResult<IEnumerable<ProductModelDTO>> Index()
+        //{
+        //    List<ProductModel> products = repository.GetAllProducts();
+        //    List<ProductModelDTO> productsDTOs = new List<ProductModelDTO>();
+
+        //    foreach (ProductModel item in products)
+        //        productsDTOs.Add(new ProductModelDTO(item));
+
+
+        //    return productsDTOs;
+        //}
+
+        [HttpGet("searchproducts/{searchTerm}")]
+        public ActionResult <IEnumerable<ProductModelDTO>> SearchResults(string searchTerm)
+        {
+            List<ProductModel> products = repository.SearchProducts(searchTerm);
+            List<ProductModelDTO> productsDTOs = new List<ProductModelDTO>();
+
+            foreach (ProductModel item in products)
+                productsDTOs.Add(new ProductModelDTO(item));
+
+
+            return productsDTOs;
+          
+        }
+
+        [HttpGet("ShowOneProduct/{Id}")]
+        public ActionResult<ProductModelDTO> ShowOneProduct(int Id)
+        {
+            ProductModel p = repository.GetProductById(Id);
+            return new ProductModelDTO(p);
+        }
+
+        [HttpPost("InsertOne")]
+        //post action
+        //expecting a product in json format in the body of the request.
+        public ActionResult <int> InsertOne(ProductModel product)
+        {
+
+            return repository.Insert(product);
+           
+        }
+
+        [HttpPut("ProcessEdit")]
+        public ActionResult <int> ProcessEdit(ProductModel product)
+        {
+            return repository.Update(product);
+
+        }
+
+        [HttpDelete("Delete/{id}")]
+        public ActionResult <int> Delete(int id)
+        {
+           
+            return repository.Delete(repository.GetProductById(id));
+            
+        }
+
+    }
+}
---
+++ ProductsApplication/Models/ProductModelDTO.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsApplication.Models
+{
+    public class ProductModelDTO
+    {
+        public ProductModelDTO(int id, string name, decimal price, string description)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Description = description;
+
+            PriceString = string.Format("{0:C}", price);
+            ShortDescription = description.Length < 25 ? description : description.Substring(0, 25);
+
+            Tax = price * 0.08M; 
+        }
+
+        public ProductModelDTO(ProductModel p)
+        {
+            Id = p.Id;
+            Name = p.Name;
+            Price = p.Price;
+            Description = p.Description;
+
+            PriceString = string.Format("{0:C}", Price);
+            ShortDescription = Description.Length < 25 ? Description : Description.Substring(0, 25);
+
+            Tax = Price * 0.08M;
+        }
+
+        [DisplayName("Id Number")]
+        public int Id { get; set; }
+
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Product Price")]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        public string PriceString { get; set; }
+
+        [DisplayName("What you get ...")]
+        public string Description { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public decimal Tax { get; set; }
+
+        
+    }
+}
